27d38be01732f789cdf8d3b2e06e00113716cbe4
Eseimokumoh <119541134+Eseimokumoh@users.noreply.github.com>
2023-08-31 06:46:48 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {    
-    apiKey: "AIzaSyCpqXzB6Yzinq4DZY317s-bKWBWpxoMaKo",
-    authDomain: "clone-209b3.firebaseapp.com",
-    projectId: "clone-209b3",
-    storageBucket: "clone-209b3.appspot.com",
-    messagingSenderId: "901032392683",
-    appId: "1:901032392683:web:f802dfe801c472b3648bae",
-    measurementId: "G-N2DL3QEKRF",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
